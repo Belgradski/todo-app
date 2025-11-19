d351c7784a6e059b1ac9f5190e30e201030fb6a2
Alexey Belgradski <Alexeybelgradski@yandex.ru>
2025-11-20 00:43:55 +0300

    features: в app: загрузка и сохранение в localStorage, добавлены состояния выбранных фильтров и тудух, добавлены функции добавления, удаления,переключения, редактирования, очистки выполненных тудух, счетчик активных тудух

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState('all');
+
+  //Загрузка из localStorage
+  useEffect(() => {
+    const saveTodos = localStorage.getItem('todos');
+    if (saveTodos) {
+      setTodos(JSON.parse(saveTodos));
+    }
+  }, []);
+
+  //сохранение в localStorage
+  useEffect(() => {
+    localStorage.setItem(JSON.stringify(todos))
+  }, [todos]);
+
+  const addTodo = (text) => {
+    if (text.trim() !== '') {
+      const newTodo = {
+        id: Date.now(),
+        text: text.trim(),
+        completed: false,
+        createAt: new Date().toLocaleDateString()
+      };
+      setTodos([...todos, newTodo]);
+    }
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(todos.map(todo =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    ));
+  };
+
+  const editTodo = (id, newText) => {
+    setTodos(todos.map(todo =>
+      todo.id === id ? { ...todo, text: newText } : todo));
+  };
+
+  const clearCompleted = () => {
+    setTodos(todos.filter(todo => !todo.completed));
+  };
+
+  const getFilteredTodos = () => {
+    switch (filter) {
+      case 'active':
+        return todos.filter(todo => !todo.completed);
+      case 'completed':
+        return todos.filter(todo => todo.completed);
+      default:
+        return todos;
+    }
+  }
+  const activeTodosCount = todos.filter(todo => !todo.completed).length;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <header className="header">
+          <h1>My Todo List</h1>
+          <p>{activeTodosCount} активных задач</p>
+        </header>
+
+      <TodoForm onAdd={addTodo} />
+
+      <Filter 
+        currentFIlter={filter}
+        onFilterChange={setFilter}
+        todosCount={todos.length}
+        activeTodosCount={activeTodosCount}
+      />
+
+      <TodoList 
+        todos={getFilteredTodos()}
+        filter={filter}
+        onToggle={toggleTodo}
+        onDelete={deleteTodo}
+        onEdit={editTodo}
+      />  
+
+      {todos.some(todo => todo.completed) && (
+        <button onClick={clearCompleted} className="clear-btn">
+          Удалить выполненые
+        </button>
+      )}
+      </div>
     </div>
-  );
+  )
+
+
 }
 
 export default App;
